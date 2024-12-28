83c3c7cc3b9fc6eaad61702a388cbdb5b1285f4d
NizamMikhalfani <nizammikhalfani2@upi.edu>
2024-12-28 15:09:49 +0700

    locations.php

--- SIGMA-P (Sistem Informasi Galeri, Museum dan Arsip - Perpustakaan)/Project #1/locations.php
+++ SIGMA-P (Sistem Informasi Galeri, Museum dan Arsip - Perpustakaan)/Project #1/locations.php
@@ -12,6 +12,26 @@ $glamLocations = [
         "lng" => 106.8219,
         "info" => "Museum Nasional Indonesia atau Museum Gajah menyimpan koleksi artefak sejarah dan budaya dari seluruh Indonesia. Terletak di Jl. Medan Merdeka Barat, Jakarta Pusat."
     ],
+    [   "name" => "Museum Fatahillah",
+        "lat" => -6.1353,
+        "lng" => 106.8204,
+        "info" => "Museum Fatahillah atau Museum Sejarah Jakarta terletak di Kota Tua, Jakarta Barat. Museum ini menampilkan koleksi yang berkaitan dengan sejarah Jakarta dan Indonesia pada masa kolonial Belanda. Alamat: Jl. Fatahillah No. 1, Jakarta Barat, DKI Jakarta."
+    ],
+    [
+        "name" => "Museum Wayang",
+        "lat" => -6.1357,
+        "lng" => 106.8197,
+        "info" => "Museum Wayang menampilkan koleksi wayang dan seni pertunjukan dari berbagai daerah di Indonesia. Museum ini terletak di Kota Tua, Jakarta Barat, tepat di sebelah Museum Fatahillah. Alamat: Jl. Pahlawan No. 2, Jakarta Barat, DKI Jakarta."
+    ],
+    [
+        "name" => "Museum Bank Indonesia",
+        "lat" => -6.1355,
+        "lng" => 106.8186,
+        "info" => "Museum Bank Indonesia adalah museum yang berfokus pada sejarah perbankan Indonesia. Terletak di Kota Tua, Jakarta Barat, museum ini menyajikan informasi mengenai perkembangan sistem keuangan dan ekonomi Indonesia. Alamat: Jl. Pahlawan No. 3, Jakarta Barat, DKI Jakarta."
+    ],
+    [
+    <!-- Tambahkan Lagi untuk Alamatnya -->
+    ],
 ];
 
 // Mengacak lokasi
